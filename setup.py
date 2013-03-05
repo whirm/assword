5e78d1afaf5e1e015aa06c32128fb15f7eca88a5
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2013-03-05 14:02:37 -0500

    added --help, --version, and version subcommands; updated setup.py with proper version info

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'assword',
-    version = '0.1',
+    version = '0.5',
     description = 'Secure password database and retrieval system.',
     author = 'Jameson Rollins',
     author_email = 'jrollins@finestructure.net',
